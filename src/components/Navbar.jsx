ca663f5309a0513657a7b12f38827c30aac05fe4
Himanshu Nayak <himanshunayak814@gmail.com>
2025-07-15 23:02:49 +0530

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="bg-blue-600 p-4 text-white flex justify-between">
-      <h1 className="text-lg font-bold">Complaint System</h1>
+      <h1 className="text-lg font-bold">Complaint System123</h1>
       <div className="space-x-4">
         <Link to="/" className="hover:underline">Home</Link>
         <Link to="/submit" className="hover:underline">Submit</Link>
